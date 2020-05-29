91c8f5ce39b31c0226b00a556aa54a0e62316cd1
johnson obaro <johnson.obaro@hotmail.com>
2020-05-29 23:08:08 +0000

    Check dispatcher thread by ID

--- Dispatcher.py
+++ Dispatcher.py
@@ -2,66 +2,73 @@
 import threading;
 import time;
 from DispatcherOperation import DispatcherOperation
-from DispatcherQueue    import DispatcherQueue;        
+from DispatcherQueue     import DispatcherQueue;        
 
+
+class Dispatcher:
+
+    __dispatchers  = list();
+    @staticmethod
+    def CreateInstance():
+        thread   = threading.current_thread();
+        result   =  None;
+        for dispatcher in Dispatcher.__dispatchers:
+            if(dispatcher.Thread  == thread):
+                result  =  dispatcher;
+                break;
+        if(result is None):
+            result  =  Dispatcher.__Dispatcher();
+            Dispatcher.__dispatchers.append(result);
+        return result;
+        
+        @staticmethod
+        def Count():
+            return len(Dispatcher.__dispatchers);
+
+    def __init__(self):
+        raise NotImplementedError("@Dispatcher does not have a constructor");
+     
+    class __Dispatcher(object):
 
         
-class Dispatcher(object):
 
-    def __init__(self):
-        self.__currentThread     =   threading.current_thread();
-        self.__DispatcherQueue   =   DispatcherQueue();
-        self.__IsRunning         =   False;
+        def __init__(self):
+            self.__currentThread     =   threading.current_thread();
+            self.__DispatcherQueue   =   DispatcherQueue();
+            self.__IsRunning         =   False;
 
+        def Invoke(self, method, *args , **kwargs):
+            operation  =  DispatcherOperation(method,*args, **kwargs)
+            self.__DispatcherQueue.Enqueue(operation);
+            return operation;
+             
+        def Run(self):
+            if(self.CheckAccess() != True):
+                raise ValueError("@Run Method : must be called on the same thread that create the dispatcher instance");
+           
+            if(self.__IsRunning == True):
+                return;
+            self.__IsRunning = True;
+            try:
+                while(self.__IsRunning is True):
+                    if(self.__DispatcherQueue.IsEmpty is not True):
+                        operation  =  self.__DispatcherQueue.Dequeue();
+                        if(operation != None):
+                            operation.Invoke();
+                    else:
+                        time.sleep(1);
+            except:
+                raise ;
+            finally:
+                self.__IsRunning = False;
 
-    def Invoke(self, method, *args , **kwargs):
-        operation  =  DispatcherOperation(method,*args, **kwargs)
-        self.__DispatcherQueue.Enqueue(operation);
-        return operation;
-         
-    def Run(self):
-        if(self.CheckAccess() != True):
-            raise ValueError("@Run Method : must be called on the same thread that create the dispatcher instance");
-       
-        if(self.__IsRunning == True):
-            return;
-        self.__IsRunning = True;
-        try:
-            while(self.__IsRunning is True):
-                if(self.__DispatcherQueue.IsEmpty is not True):
-                    operation  =  self.__DispatcherQueue.Dequeue();
-                    if(operation != None):
-                        operation.Invoke();
-                else:
-                    time.sleep(1);
-        except:
-            raise ;
-        finally:
-            self.__IsRunning = False;
+        def CheckAccess(self):
+            return (threading.current_thread().ident == self.Thread.ident);
 
-    def CheckAccess(self):
-        return (threading.current_thread() == self.Thread);
+        @property
+        def Thread(self):
+            return self.__currentThread;
 
-    @property
-    def Thread(self):
-        return self.__currentThread;
-
-
-class DispatcherObject(object):
-
-
-    def __init__(self):
-        self.__Dispatcher   = None;
-
-    @property
-    def Dispatcher(self):
-        return self.__Dispatcher;
-
-    def CheckAccess(self):
-        status  =  False;
-        if(self.Dispatcher != None):
-            status  =  sellf.Dispatcher.CheckAccess();
-        return status;
     
        
 
@@ -86,7 +93,8 @@ if(__name__ =="__main__"):
             a =  a + 1;
 
           
-    dispatcher  =  Dispatcher();
+    dispatcher  =  Dispatcher.CreateInstance();
+ 
     dispatcher.Invoke(Task1);
     dispatcher.Invoke(Task2, 5, 6);
     print(dispatcher.Thread);
